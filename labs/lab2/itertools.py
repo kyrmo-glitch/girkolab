1c3198964205739a8a332eadd77f1c94aec053f3
kyrmo-glitch <alexgirko77@mail.ru>
2026-02-16 23:17:04 +0500

    lab2

---
+++ labs/lab2/itertools.py
@@ -0,0 +1,10 @@
+from itertools import product
+
+letters = "ТИМОФЕЙ"
+count = 0
+for x in product(letters, repeat=5):
+    s = ''.join(x)
+    if s.count('Й') <= 1 and s[0] != 'Й' and s[-1] != 'Й' and 'ИЙ' not in s and 'ЙИ' not in s:
+        count += 1
+
+print(count)
