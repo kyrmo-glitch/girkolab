215084dcd08d8585d6a635f0a3dd5ce7f032e340
kyrmo-glitch <alexgirko77@mail.ru>
2026-02-17 17:39:43 +0500

    lab2

--- labs/lab2/itertools2.py
+++ labs/lab2/itertools2.py
@@ -1,11 +1,10 @@
 def count_ones_in_binary():
-    value = 4**2020 + 2**2017 - 15
+    x = 4**2020 + 2**2017 - 15
     count = 0
-    n = value
-    while n > 0:
-        if n % 2 == 1:  
+    while x > 0:
+        if x % 2 == 1:  
             count += 1
-        n //= 2          
+        x //= 2          
     return count
 
 result = count_ones_in_binary()
