58016d4597020cfcc699950fea6ad41a8a197b76
kyrmo-glitch <alexgirko77@mail.ru>
2026-02-19 14:06:40 +0500

    Update itertools3.py

--- labs/lab2/itertools3.py
+++ labs/lab2/itertools3.py
@@ -1,14 +1,14 @@
-def search_div(n):
-    divisors = set()
+def searchediv(n):
+    div = set()
     for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
-            divisors.add(i)
-            divisors.add(n//i)
-        if len(divisors) > 2:
+            div.add(i)
+            div.add(n//i)
+        if len(div) > 2:
             break
-    return divisors
+    return div
 
 for i in range(174457, 174506):
-    d = sorted(search_div(i))
+    d = sorted(searchediv(i))
     if len(d) == 2:
         print(d[0], d[1])
