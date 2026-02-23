97a1ec7a174b7f747d61f13c8ef93e93127dfc11
kyrmo-glitch <alexgirko77@mail.ru>
2026-02-23 21:28:48 +0500

    Update itertools2.py

--- labs/lab2/itertools2.py
+++ labs/lab2/itertools2.py
@@ -6,12 +6,6 @@ def countone():
            count += 1
         x //= 2          
     return count
-<<<<<<< HEAD
-s = bin(4**2020 + 2**2017 - 15)
-print(s.count('1'))
-result = count_ones_in_binary()
-print(f"Количество единиц {result}")
-=======
-
+    
 print(countone())
->>>>>>> 19e7fbb87d394623c8b702a5227b7b7c90974072
+
