e798029636c6b5b482f26d0e37e45f0af4e119df
kyrmo-glitch <alexgirko77@mail.ru>
2026-02-25 23:22:42 +0500

    Update README files with lab results

--- labs/lab3/recursion.py
+++ labs/lab3/recursion.py
@@ -5,4 +5,8 @@ def count(lst):
         if isinstance(item, list):
             total += count(item)
     return total
+
+print(count([]))
+print(count([1, 2, 3]))
+print(count(["x", "y", ["z"]]))
 print(count([1, 2, [3, 4, [5]]]))  
