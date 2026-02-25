fe743f9ab429e2d25df8c683220021dc5405480d
kyrmo-glitch <alexgirko77@mail.ru>
2026-02-25 20:19:41 +0500

    lab3

--- labs/lab3/recursion.py
+++ labs/lab3/recursion.py
@@ -1,9 +1,8 @@
 def count(lst):
     total = 0
     for item in lst:
+        total +=1
         if isinstance(item, list):
             total += count(item)
-        else:
-            total += 1
     return total
 print(count([1, 2, [3, 4, [5]]]))  
