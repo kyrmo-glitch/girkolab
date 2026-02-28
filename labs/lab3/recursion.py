62af8aa0e9e0012a05bfddad9f57d32d6cb79bbc
kyrmo-glitch <alexgirko77@mail.ru>
2026-02-28 20:31:38 +0500

    lab3

--- labs/lab3/recursion.py
+++ labs/lab3/recursion.py
@@ -9,4 +9,32 @@ def count(lst):
 print(count([]))
 print(count([1, 2, 3]))
 print(count(["x", "y", ["z"]]))
-print(count([1, 2, [3, 4, [5]]]))  
+print(count([1, 2, [3, 4, [5]]]))
+
+
+print('-------------------------------')
+
+
+def count_elements(lst):
+    process = [lst]  # список списков для обработки
+    count = 0
+    
+    while process:
+        # Берем первый элемент (не последний, как в стеке)
+        current = process[0]
+        process = process[1:]  # удаляем первый элемент
+        
+        for item in current:
+            if isinstance(item, list):
+                process.append(item)  # добавляем в конец
+                count += 1
+            else:
+                count += 1
+    
+    return count
+
+# Проверка
+print(count_elements([]))                    # 0
+print(count_elements([1, 2, 3]))             # 3
+print(count_elements(["x", "y", ["z"]]))     # 4
+print(count_elements([1, 2, [3, 4, [5]]]))   # 7
