87ad23d059aa26f5cc602b152cb42fc2e0330770
kyrmo-glitch <alexgirko77@mail.ru>
2026-04-02 10:33:07 +0500

    lab6

--- labs/lab6/main.py
+++ labs/lab6/main.py
@@ -1,4 +1,4 @@
-import tkinter as tk
+from tkinter import *
 from tkinter import messagebox, filedialog
 from docx import Document
 from figure_pkg import *
@@ -14,12 +14,12 @@ def calculate_rectangle():
     ri = rect_r_in(a, b)
     ro = rect_r_out(a, b)
     
-    text = f"Прямоугольник\nСтороны: {a} и {b}\nПлощадь: {s}\n"
+    text = f"Прямоугольник\nСтороны: {a} и {b}\nПлощадь: {round(s,2)}\n"
     if ri > 0:
-        text += f"Радиус вписанной: {ri}\n"
+        text += f"Радиус вписанной: {round(ri,2)}\n"
     else:
         text += "Вписанной окружности нет\n"
-    text += f"Радиус описанной: {ro}"
+    text += f"Радиус описанной: {round(ro,2)}"
     
     rect_result.set(text)
     global last_rect_result
@@ -64,78 +64,76 @@ def save_all():
     file_path = filedialog.asksaveasfilename(defaultextension=".docx")
     if file_path:
         doc.save(file_path)
-        messagebox.showinfo("Успех", "Файл сохранён!")
+        messagebox.showinfo("!!!","Файл сохранён!")
 
-root = tk.Tk()
+root = Tk()
 root.title("Калькулятор геометрических фигур")
-root.geometry("900x500")
+root.geometry("750x500")
 
 
-left_frame = tk.LabelFrame(root, text="Прямоугольник", font=("Arial", 12, "bold"),padx=10, pady=10)
-left_frame.pack(side="left", fill="both", expand=True, padx=5, pady=5)
+left_frame = LabelFrame(root, text="Прямоугольник", font=("Arial", 12, "bold"))
+left_frame.pack(side="left", fill="both", expand=False, padx=10, pady = 10)
 
-tk.Label(left_frame, text="Сторона a:").grid(row=0, column=0, pady=5)
-rect_a_entry = tk.Entry(left_frame, width=10)
-rect_a_entry.grid(row=0, column=1, pady=5)
+Label(left_frame, text="Сторона a:").grid(row=0, column=0, pady=10)
+rect_a_entry = Entry(left_frame, width=10)
+rect_a_entry.grid(row=0, column=1, pady=10)
 
-tk.Label(left_frame, text="Сторона b:").grid(row=1, column=0, pady=5)
-rect_b_entry = tk.Entry(left_frame, width=10)
-rect_b_entry.grid(row=1, column=1, pady=5)
+Label(left_frame, text="Сторона b:").grid(row=1, column=0, pady=10)
+rect_b_entry = Entry(left_frame, width=10)
+rect_b_entry.grid(row=1, column=1, pady=10)
 
-tk.Button(left_frame, text="Рассчитать", command=calculate_rectangle).grid(row=2, column=0, columnspan=2, pady=10)
+Button(left_frame, text="Рассчитать",command=calculate_rectangle).grid(row=2, column=0, columnspan=2, padx = 10, pady=10)
 
-rect_result = tk.StringVar()
+rect_result = StringVar()
 rect_result.set("Результат")
-tk.Label(left_frame, textvariable=rect_result, justify="left", padx=5, pady=5).grid(row=3, column=0, columnspan=2, pady=10)
+Label(left_frame, textvariable=rect_result, justify="left",pady=5).grid(row=3, column=0, columnspan=2, pady=10)
 
 
-center_frame = tk.LabelFrame(root, text="Треугольник", font=("Arial", 12, "bold"), padx=5, pady=5)
-center_frame.pack(side="left", fill="both", expand=True, padx=5, pady=5)
+center_frame = LabelFrame(root, text="Треугольник", font=("Arial", 12, "bold"))
+center_frame.pack(side="left", fill="both", expand=False, padx=10, pady=10)
 
-tk.Label(center_frame, text="Сторона a:").grid(row=0, column=0, pady=5)
-tri_a_entry = tk.Entry(center_frame, width=10)
+Label(center_frame, text="Сторона a:").grid(row=0, column=0, pady=5)
+tri_a_entry = Entry(center_frame, width=10)
 tri_a_entry.grid(row=0, column=1, pady=5)
 
-tk.Label(center_frame, text="Сторона b:").grid(row=1, column=0, pady=5)
-tri_b_entry = tk.Entry(center_frame, width=10)
+Label(center_frame, text="Сторона b:").grid(row=1, column=0, pady=5)
+tri_b_entry = Entry(center_frame, width=10)
 tri_b_entry.grid(row=1, column=1, pady=5)
 
-tk.Label(center_frame, text="Сторона c:").grid(row=2, column=0, pady=5)
-tri_c_entry = tk.Entry(center_frame, width=10)
+Label(center_frame, text="Сторона c:").grid(row=2, column=0, pady=5)
+tri_c_entry = Entry(center_frame, width=10)
 tri_c_entry.grid(row=2, column=1, pady=5)
 
-tk.Button(center_frame, text="Рассчитать", command=calculate_triangle).grid(row=3, column=0,columnspan=2, pady=10)
+Button(center_frame, text="Рассчитать", command = calculate_triangle).grid(row=3, column=0,columnspan=2, pady=10)
 
-tri_result = tk.StringVar()
+tri_result = StringVar()
 tri_result.set("Результат")
-tk.Label(center_frame, textvariable=tri_result, padx=5, pady=5).grid(row=4, column=0,columnspan=2, pady=10)
+Label(center_frame, textvariable=tri_result,justify='left', pady=5).grid(row=4, column=0,columnspan=2, pady=10)
 
 
-right_frame = tk.LabelFrame(root, text="Трапеция", font=("Arial", 12, "bold"), padx=10, pady=10)
-right_frame.pack(side="left", fill="both", expand=True, padx=5, pady=5)
+right_frame = LabelFrame(root, text="Трапеция", font=("Arial", 12, "bold"))
+right_frame.pack(side="left", fill="both", expand=False, padx=10, pady=10)
 
-tk.Label(right_frame, text="Основание a:").grid(row=0, column=0, pady=5)
-trap_a_entry = tk.Entry(right_frame, width=10)
-trap_a_entry.grid(row=0, column=1, pady=5)
+Label(right_frame, text="Основание a:").grid(row=0, column=0, pady=10)
+trap_a_entry = Entry(right_frame, width=10)
+trap_a_entry.grid(row=0, column=1, pady=10)
 
-tk.Label(right_frame, text="Основание b:").grid(row=1, column=0, pady=5)
-trap_b_entry = tk.Entry(right_frame, width=10)
-trap_b_entry.grid(row=1, column=1, pady=5)
+Label(right_frame, text="Основание b:").grid(row=1, column=0, pady=10)
+trap_b_entry =Entry(right_frame, width=10)
+trap_b_entry.grid(row=1, column=1, pady=10)
 
-tk.Label(right_frame, text="Высота h:").grid(row=2, column=0, pady=5)
-trap_h_entry = tk.Entry(right_frame, width=10)
-trap_h_entry.grid(row=2, column=1, pady=5)
+Label(right_frame, text="Высота h:").grid(row=2, column=0, pady=10)
+trap_h_entry = Entry(right_frame, width=10)
+trap_h_entry.grid(row=2, column=1, pady=10)
 
-tk.Button(right_frame, text="Рассчитать", command=calculate_trapezoid).grid(row=3, column=0, columnspan=2, pady=10)
+Button(right_frame, text="Рассчитать", command = calculate_trapezoid).grid(row=3, column=0, columnspan=2, pady=10)
 
-trap_result = tk.StringVar()
+trap_result = StringVar()
 trap_result.set("Результат")
-tk.Label(right_frame, textvariable=trap_result, justify="left", padx=5, pady=5).grid(row=4, column=0, columnspan=2, pady=10)
+Label(right_frame, textvariable=trap_result,justify="left", padx=5, pady=5).grid(row=4, column=0, columnspan=2, pady=10)
 
 
-bottom_frame = tk.Frame(root)
-bottom_frame.pack(side="bottom", fill="x", pady=10)
-
-tk.Button(bottom_frame, text="Сохранить все результаты в Word", command=save_all, font=("Arial", 10, "bold")).pack()
+btn = Button(root, text="Сохранить в Word",command=save_all, font=("Arial", 10, "bold"))
+btn.pack(side = RIGHT,pady = 65, padx = 15)
 
 root.mainloop()
