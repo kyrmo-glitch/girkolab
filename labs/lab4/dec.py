c7e748338002e15590ed3f2326a0b54e2d9cb109
kyrmo-glitch <alexgirko77@mail.ru>
2026-04-08 22:33:13 +0500

    lab4

--- labs/lab4/dec.py
+++ labs/lab4/dec.py
@@ -1,15 +1,15 @@
-def repeat(times):
-    def decorator(func):
-        def wrapper(*args):
-            results = []
-            for i in range(times):
-                result = func(*args)
-                results.append(result)
-            return results
-        return wrapper
-    return decorator
+a = int(input())
+def repeat(decorated_func):
+    def wrapper(*args):
+        global a
+        results = []
+        for i in range(a):  
+            result = decorated_func(*args)
+            results.append(result)
+        return results
+    return wrapper
 
-@repeat(times=3)
+@repeat
 def say_hello(name):
     return f"Привет, {name}!"
 
